7429928248b2767080a6535157e6caa9d5eeb91d
Sebastian Madejski <209388@edu.p.lodz.pl>
2019-04-24 16:40:17 +0200

    make messages saveable in DB

--- src/main/java/pl/mwht/websocket/WebSocketHandler.java
+++ src/main/java/pl/mwht/websocket/WebSocketHandler.java
@@ -1,21 +1,45 @@
 package pl.mwht.websocket;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import pl.mwht.entity.ChatRecord;
+import pl.mwht.repository.ChatRecordRepository;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Component
 public class WebSocketHandler extends TextWebSocketHandler {
 
+    @Autowired
+    private ChatRecordRepository chatRecordRepository;
+
+    private Map<WebSocketSession, String> usersInRoomsBinding = new HashMap<WebSocketSession, String>();
+
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String messagePayload = message.getPayload();
         Logger.getAnonymousLogger().log(Level.INFO, "Received payload: " + messagePayload);
+
+        JSONObject inboundObject = new JSONObject(messagePayload);
+        try {
+            String roomId = inboundObject.getString("roomId");
+            String mesg = inboundObject.getString("mesg");
+
+            ChatRecord chatRecord = new ChatRecord(null, roomId, mesg);
+            chatRecordRepository.save(chatRecord);
+        } catch (JSONException jsone) {
+            Logger.getAnonymousLogger().log(Level.SEVERE, jsone.toString());
+            jsone.printStackTrace();
+        }
     }
 
     @Override
