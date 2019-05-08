8f670153587e06b1872349c464c4c8b3af3759f0
Sebastian Madejski <madejski.s97@gmail.com>
2019-05-08 17:30:21 +0200

    add query method for removing chat record by room id

--- src/main/java/pl/mwht/repository/ChatRecordRepository.java
+++ src/main/java/pl/mwht/repository/ChatRecordRepository.java
@@ -1,7 +1,10 @@
 package pl.mwht.repository;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import pl.mwht.entity.ChatRecord;
 
 import java.util.List;
@@ -9,4 +12,9 @@ import java.util.List;
 @Repository
 public interface ChatRecordRepository extends CrudRepository<ChatRecord, Integer> {
     public List<ChatRecord> findChatRecordsByRoomId(String roomId);
+
+    @Modifying
+    @Transactional
+    @Query("delete from ChatRecord cr where cr.roomId = ?1")
+    public void deleteChatRecordsByRoomId(String roomId);
 }
